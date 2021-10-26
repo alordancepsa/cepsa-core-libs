bdfe4e0abe7a938b458af2b1a9ad4c28d37e204a
Angel Lordan <angel.lordan@outsourcing.cepsa.com>
2021-10-26 19:40:24 +0200

    fixing .gitignore

--- core_libs/s3/deltas/__init__.py
+++ core_libs/s3/deltas/__init__.py
@@ -1,3 +1,3 @@
 from core_libs.s3.deltas.sets_diff import delta_new as delta_new
 from core_libs.s3.deltas.sets_diff import delta_delete as delta_delete
-from core_lons.s3.deltas.sets_diff import delta_update as delta_update
+from core_libs.s3.deltas.sets_diff import delta_update as delta_update
--- core_libs/s3/deltas/csv/__init__.py
+++ core_libs/s3/deltas/csv/__init__.py
@@ -1,2 +1,2 @@
-from core_libs.s3.deltas.fast_delta import generate as fast_delta
-from core_libs.s3.deltascore_libs.s3.deltas.memory_efficient_delta import generate as memory_efficient_delta
+from core_libs.s3.deltas.csv.fast_delta import generate as fast_delta
+from core_libs.s3.deltas.csv.memory_efficient_delta import generate as memory_efficient_delta
